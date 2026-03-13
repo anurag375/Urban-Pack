9c648cfecdf7badc26ddbe6f00dbedc8aa045f97
Anurag375 <anurag375.2020@gmail.com>
2026-03-14 00:02:18 +0530

    phase4: configure MongoDB connection using config and debug; add config/development.json

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,13 +1,22 @@
 const mongoose = require('mongoose');
+const config = require("config");
+
+const dbgr = require("debug")("development:mongoose");
 
 mongoose
-.connect('mongodb://127.0.0.1:27017/Urban-Pack')    // written till developement phase  // professionally, we don't write the localhost URL in the code, instead we write it in the .env file and access it using process.env.DB_URL
-// .connect(process.env.DB_URL)
+// mongodb://127.0.0.1:27017
+// .connect('/Urban-Pack')    // --> written till developement phase  // professionally, we don't write the localhost URL in the code, instead we write it in the .env file and access it using process.env.DB_URL
+// --> .connect(process.env.DB_URL)
+.connect(config.get("MONGODB_URI") + "/Urban-Pack")    // using config module to access the MongoDB URI from the development.json file.  // we can also use dotenv module to access the environment variables from the .env file.
+// --> or .connect(`${config.get("MONGODB_URI")}/Urban-Pack`)    
+
 .then(function(){
-    console.log("connected");
+    // console.log("connected");
+    dbgr("connected to MongoDB successfully!");    // using debug module to log the message instead of console.log() method.  // we can also use chalk module to color the log messages.
 })
 .catch(function(err){
-    console.log(err);
+    // console.log(err);
+    dbgr(err);
 })
 
 module.exports = mongoose.connection;
