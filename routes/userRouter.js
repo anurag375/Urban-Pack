8ed7937cb348ebed8583d6f1c3ae231b8d21a884
Anurag375 <anurag375.2020@gmail.com>
2026-03-15 23:57:40 +0530

    phase5: add user registration route

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,10 +1,45 @@
 const express = require('express');
 const router = express.Router();
+const userModel = require('../models/user-model');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 router.get("/", (req, res) => {
     res.send("hey it's working!");
 });
 
 console.log(process.env.NODE_ENV, "from users");
+
+router.post("/register", (req, res) => {
+    try{
+        let { fullname, email, password } = req.body;
+
+        bcrypt.genSalt(10, function(err, salt) {    // creating salt (random string)
+            bcrypt.hash(password, salt, async function(err, hash) {
+                if(err) return res.send(err.message);
+                else {
+                    res.send(hash);
+
+                    // let user = await userModel.create({
+                    //     fullname,
+                    //     email,
+                    //     password: hash
+                    // });
+                    // res.send(user);
+                    
+                    // let token = jwt.sign({email, id: user._id}, 'shhhhhhhhhh');
+                    // res.send(token);
+                    // res.cookie("token", token);
+                    // res.send(token);
+                }
+            });
+        });
+
+
+    } catch(err) {
+        res.send(err.message);
+    }
+});
+
 
 module.exports = router;
